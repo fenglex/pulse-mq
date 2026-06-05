d40f494affc372cafc25aadf62d73501d1fdd249
haifeng <fenglex@126.com>
2026-06-05 20:29:58 +0800

    完善 __init__.py 导出核心 API，Phase 1-5 全部完成

--- src/pulsemq/__init__.py
+++ src/pulsemq/__init__.py
@@ -1,1 +1,14 @@
-"""PulseMQ - 高性能金融行情消息中间件"""
+"""PulseMQ - 高性能金融行情消息中间件。"""
+
+from pulsemq.server import PulseServer
+from pulsemq.client.async_client import PulseClient, PulseMessage, PulseError
+from pulsemq.config import BrokerConfig, load_config
+
+__all__ = [
+    "PulseServer",
+    "PulseClient",
+    "PulseMessage",
+    "PulseError",
+    "BrokerConfig",
+    "load_config",
+]
